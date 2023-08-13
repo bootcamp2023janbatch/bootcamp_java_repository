6f0d6e1e672af7baff25c8fd530390235e47e4c1
NehaDelphin <misc@delphins.us>
2023-08-13 11:54:36 -0500

    customer fixed

--- Customer.java
+++ Customer.java
@@ -1,43 +1,5 @@
-package com.estore.bean;
-
-import java.io.Serializable;
-// This comment written akash
-public class Customer implements Serializable{
-	int id;
-	String name;
-	String email;
-	public Customer(int id, String name, String email) {
-		
-		this.id = id;
-		this.name = name;
-		this.email = email;
-	}
-	public Customer() {
-		
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", name=" + name + ", email=" + email + "]";
-	}
-	public String sayHello() {
-		return "Hello, "+name;
-	}
+class Customer {
+    public static void main(String args[]){
+        System.out.println("Welcome to bootcamp batch - Customer");
+    }
 }
