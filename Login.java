f793997cd8df8f6dfea16e23d7b193a8c5e0808d
ognjenmedic <ognjen.medic@gmail.com>
2023-08-13 19:12:42 +0200

    error fixed

--- Login.java
+++ Login.java
@@ -2,7 +2,7 @@ class Login {
     public static void main(String args[]){
         System.out.println("Welcome to Login");
         // ognjenmedic added comment
-        System.out.println("Welcome to Login;
+        System.out.println("Welcome to Login");
         // added error
 
     }
