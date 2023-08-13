3fdfadb4c70b230cd29868e85f285c85a646b7a5
ognjenmedic <ognjen.medic@gmail.com>
2023-08-13 19:03:52 +0200

    with error

--- Login.java
+++ Login.java
@@ -2,5 +2,8 @@ class Login {
     public static void main(String args[]){
         System.out.println("Welcome to Login");
         // ognjenmedic added comment
+        System.out.println("Welcome to Login;
+        // added error
+
     }
 }
